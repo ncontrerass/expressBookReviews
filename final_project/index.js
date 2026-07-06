66f170f52a112baa47a211b97ce4945574e98e3a
ncontrerass <50805848+ncontrerass@users.noreply.github.com>
2026-07-06 16:51:23 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -13,7 +13,7 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 app.use("/customer/auth/*", function auth(req,res,next){
     // Check if user is logged in and has valid access token
     if (req.session && req.session.authorization) {
-        let token = req.session.authorization.accessToken; //['accessToken'];
+        let token = req.session.authorization['accessToken'];
 
         // Verify JWT token
         jwt.verify(token, "access", (err, user) => {
